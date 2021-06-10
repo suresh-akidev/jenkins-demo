6363501e9ae4929f033ace5162d9b337a1867fe1
suresh-akidev <48638555+suresh-akidev@users.noreply.github.com>
2021-06-10 22:01:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "gcs" {
-  bucket = "my-tfstate-bucket"   # GCS bucket name to store terraform tfstate
+  bucket = "xyz-sur"   # GCS bucket name to store terraform tfstate
   prefix = "first-app"           # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
   }
 }
